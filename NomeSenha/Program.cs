166e619d05dbc96f565478dd37bdef107b2c30c0
joao3313 <ikkijp2012@gmail.com>
2025-02-19 19:39:07 -0300

    feat: NomeSenha

---
+++ NomeSenha/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace NomeSenha
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            string usuario, senha;
+
+            while (true)
+            {
+                Console.Write("Digite o nome de usuário: ");
+                usuario = Console.ReadLine();
+
+                Console.Write("Digite a senha: ");
+                senha = Console.ReadLine();
+
+                if (senha.Equals(usuario, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Erro: A senha não pode ser igual ao nome de usuário. Tente novamente.\n");
+                }
+                else
+                {
+                    Console.WriteLine("Usuário e senha cadastrados com sucesso!");
+                    break;
+                }
+            }
+        }
+    }
+}
