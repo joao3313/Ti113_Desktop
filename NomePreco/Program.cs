d3365fdf6abbf0ae352d61a59ae99d293b78ea2a
joao3313 <ikkijp2012@gmail.com>
2025-02-19 19:18:16 -0300

    feat: NomePreco

---
+++ NomePreco/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace NomePreco
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            string[] nomes = new string[6];
+            double[] precos = new double[6];
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write($"Digite o nome do produto {i + 1}: ");
+                nomes[i] = Console.ReadLine();
+
+                Console.Write($"Digite o preço do produto {i + 1}: ");
+                while (!double.TryParse(Console.ReadLine(), out precos[i]) || precos[i] < 0)
+                {
+                    Console.Write("Preço inválido. Digite novamente: ");
+                }
+            }
+
+            // Exibir o nome do último produto inserido
+            Console.WriteLine($"\nO último produto inserido foi: {nomes[5]}");
+
+            // Encontrar o produto mais barato
+            int indiceMaisBarato = 0;
+            for (int i = 1; i < 6; i++)
+            {
+                if (precos[i] < precos[indiceMaisBarato])
+                {
+                    indiceMaisBarato = i;
+                }
+            }
+
+            Console.WriteLine($"O produto mais barato é: {nomes[indiceMaisBarato]} - R$ {precos[indiceMaisBarato]:F2}");
+        }
+    }
+}
