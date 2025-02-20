08d8768df138f2fbd8ca1b1385aeee111bc93270
joao3313 <ikkijp2012@gmail.com>
2025-02-20 19:02:15 -0300

    Feat: Encerramento

---
+++ Encerramento/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Encerramento
+{
+    using System;
+    using System.Threading;
+
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                DateTime agora = DateTime.Now;
+                Console.Clear();
+                Console.WriteLine("Relógio Atualizado: " + agora.ToString("HH:mm:ss"));
+
+                if (agora.Hour == 22 && agora.Minute == 15)
+                {
+                    Console.WriteLine("\n*** ALARME: São 22:15! ***");
+                    Console.Beep();
+                    break;
+                }
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
